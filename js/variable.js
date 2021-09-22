434f093e2d7dae692e655c21bfe3741cc732b229
carlosmendezs <carlosmen1987@gmail.com>
2021-09-22 13:06:06 -0700

    agregar interfaz de cambio de clave y corrección de estilos

--- js/variable.js
+++ js/variable.js
@@ -1,12 +1,11 @@
+var credencial = new Map();
+    credencial.set("administrador","admin")
+    credencial.set("operario","operario")
+    credencial.set("vendedeor","vendedor")
 
 function obtenerdatos(){
     var usuario = document.getElementById('user').value;
     var contraseña = document.getElementById('password').value;
-    
-    let credencial = new Map();
-    credencial.set("administrador","admin")
-    credencial.set("operario","operario")
-    credencial.set("vendedeor","vendedor")
     
     if (credencial.has(usuario)){
     	if (contraseña == credencial.get(usuario)){
@@ -23,7 +22,36 @@ function obtenerdatos(){
     }
 }
 
-function cambiarContraseña(){
-    alert("va a cambiar la contraseña")
-
+function cambiarContrasena(){
+    var contrasenaOld = document.getElementById('password-old').value;
+    var contrasenaNew = document.getElementById('password-new').value;
+    var contrasenaCopy = document.getElementById('password-copy').value;
+     
+    let contador = 0
+    let contadorU = 0
+    for (var contrasena of credencial.values() ){
+      if (contrasena == contrasenaOld){
+          for (var usuario of credencial.keys()){
+              if (contador == contadorU){
+                  break
+              }
+            contadorU += 1
+            }
+          break
+      }
+      contador += 1
+    }
+ 
+    if (contador < credencial.size){
+        if (contrasenaNew == contrasenaCopy){
+            credencial.set(usuario,contrasena)
+            alert("Contraseña cambiada correctamente")
+            window.close();
+            return false;
+        }else{
+            alert("su nueva contraseña no coincide")
+        }
+    }else{
+        alert("su contraseña actual es incorrecta")
+    }
 }
