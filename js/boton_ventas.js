e099d9456c2454da6a536914c6f6dd5b4105c3b2
JuanMa <ingjuanmasuarez@gmail.com>
2021-09-24 08:43:37 -0500

    Unificacion de los CSS y las interfaces de usuario. Cambios en el menu lateral, footer y header

---
+++ js/boton_ventas.js
@@ -0,0 +1,11 @@
+function ingresarPedido(){
+  
+  var venta_id = document.getElementById('venta_id').value;
+  var venta_total = document.getElementById('venta_total').value;
+  var detalle = document.getElementById('detalle').value;
+  var fecha_de_pago = document.getElementById('fecha_de_pago').value;
+  var fecha_de_pago_futura = document.getElementById('fecha_de_pago_futura').value;
+  var responsable = document.getElementById('responsable').value;
+
+  alert("Pedido ingresado satisfactoriamente");
+}
