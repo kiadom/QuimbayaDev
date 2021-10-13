0e57f14a4d227042200b815d3ab4796743e5ab88
kiadom <kiadom777@gmail.com>
2021-10-13 12:21:30 -0500

    cambios ventas

--- Frontend/src/pages/admin_ventas.jsx
+++ Frontend/src/pages/admin_ventas.jsx
@@ -108,8 +108,9 @@ const TablaVentas = ({listaVentas})=> {
                         <th>Total Venta</th>
                         <th>Fecha Venta</th>
                         <th>Fecha De Pago</th>
-                        <th>Fecha De Pago Futura</th>
-                        <th>Responsable</th>
+                        <th>Cliente ID</th>
+                        <th>Nombre Cliente</th>
+                        <th>Vendedor</th>
                         <th>Acciones</th>
                     </tr>
                 </thead>
@@ -132,21 +133,29 @@ const FilaVenta = ({venta}) => {
             {edit?(
                 <>
                     <td>{venta.venta_id}</td>
-                    <td>{venta.venta_total}</td>
                     <td>{venta.detalle}</td>
-                    <td>{venta.fecha_de_pago}</td>
-                    <td>{venta.fecha_de_pago_futura}</td>
-                    <td>{venta.responsable}</td>
+                    <td>{venta.cantidad}</td>
+                    <td>{venta.precio_unitario_por_producto}</td>
+                    <td>{venta.venta_total}</td>
+                    <td>{venta.fecha_venta}</td>
+                    <td>{venta.client_id}</td>
+                    <td>{venta.nombre_client}</td>
+                    <td>{venta.vendedor}</td>
+                    <td>{venta.estado}</td>
                     
                 </>
                 ):(
                 <>
                     <td>{venta.venta_id}</td>
-                    <td>{venta.venta_total}</td>
                     <td>{venta.detalle}</td>
-                    <td>{venta.fecha_de_pago}</td>
-                    <td>{venta.fecha_de_pago_futura}</td>
-                    <td>{venta.responsable}</td>
+                    <td>{venta.cantidad}</td>
+                    <td>{venta.precio_unitario_por_producto}</td>
+                    <td>{venta.venta_total}</td>
+                    <td>{venta.fecha_venta}</td>
+                    <td>{venta.client_id}</td>
+                    <td>{venta.nombre_cliente}</td>
+                    <td>{venta.vendedor}</td>
+                    <td>{venta.estado}</td>
                 </>
             )}
 
@@ -163,7 +172,7 @@ const FilaVenta = ({venta}) => {
                                 
                 )}
                         
-                    <div className="boton_delete">
+                    <div onClick={()=>className="boton_delete">
                         <FontAwesomeIcon icon={faTrash}/>
                     </div>
             </td>
@@ -191,19 +200,28 @@ const FormularioCreacionVentas = ({setMostrarTabla, listaVentas, setVentas })=> 
             headers: {'Content-Type': 'application/json'},
             data: {
               venta_id: nuevaVenta.venta_id,
-              venta_total: nuevaVenta.venta_total,
               detalle: nuevaVenta.detalle,
-              fecha_de_pago: nuevaVenta.fecha_de_pago,
-              fecha_de_pago_futura: nuevaVenta.fecha_de_pago_futura,
-              responsable: nuevaVenta.responsable,
+              cantidad: nuevaVenta.cantidad,
+              precio_unitario_por_producto: nuevaVenta.precio_unitario_por_producto,
+              venta_total: nuevaVenta.venta_total,
+              fecha_venta: nuevaVenta.fecha_venta,
+              client_id: nuevaVenta.client_id,
+              nombre_cliente: nuevaVenta.nombre_cliente,
+              vendedor: nuevaVenta.vendedor,
+              estado: nuevaVenta.estado,
             }
           };
           
-          axios.request(options).then(function (response) {
+        await axios
+        .request(options)
+        .then(function (response) {
             console.log(response.data);
-          }).catch(function (error) {
-            console.error(error);
-          });
+            toast.success("Venta agregada con exito")
+        })
+        .catch(function (error) {
+           console.error(error);
+           toast.error("Error al crear venta");
+        });
 
         //setMostrarTabla(true)
         //console.log("Datos del Form Enviados", nuevoUsuario);
@@ -225,31 +243,53 @@ const FormularioCreacionVentas = ({setMostrarTabla, listaVentas, setVentas })=> 
                     </tr>
 
                     <tr>
-                        <td><p>Venta Total:</p></td>
-                        <td><input 
-                            name="venta_total" 
-                            className="input_m" 
-                            type="text"
-                            placeholder="Venta Total" 
-                            required/>
-                        </td>
-                    </tr>
-                
-                    <tr>
                         <td><p>Detalle:</p></td>
                         <td><input 
                             name="detalle" 
                             className="input_m" 
                             type="text"
-                            placeholder="detalle" 
+                            placeholder="Detalle" 
+                            required/>
+                        </td>
+                    </tr>
+                
+                    <tr>
+                        <td><p>Cantidad:</p></td>
+                        <td><input 
+                            name="cantidad" 
+                            className="input_m" 
+                            type="text"
+                            placeholder="Cantidad" 
                             required/>
                         </td>
                     </tr>
                     
                     <tr>
-                        <td><p>Fecha de Pago::</p></td>
+                        <td><p>Valor Producto:</p></td>
                         <td><input 
-                            name="fecha_de_pago" 
+                            name="precio_unitario_por_producto" 
+                            className="input_m" 
+                            type="text"
+                            placeholder="Valor Producto"
+                            required/>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td><p>Total Venta:</p></td>
+                        <td><input 
+                            name="venta_total" 
+                            className="input_m" 
+                            type="text"
+                            placeholder="Total Venta"
+                            required/>
+                        </td>
+                    </tr>
+                    
+                    <tr>
+                        <td><p>Fecha Venta:</p></td>
+                        <td><input 
+                            name="fecha_venta" 
                             className="input_m" 
                             type="date"
                             required/>
@@ -257,24 +297,51 @@ const FormularioCreacionVentas = ({setMostrarTabla, listaVentas, setVentas })=> 
                     </tr>
 
                     <tr>
-                        <td><p>Fecha de Pago Futura:</p></td>
+                        <td><p>Cliente ID:</p></td>
                         <td><input 
-                            name="fecha_de_pago_futura" 
+                            name="cliente_id" 
                             className="input_m" 
-                            type="date"
+                            type="text"
+                            placeholder="ID Cliente"
                             required/>
                         </td>
                     </tr>
-                    
+
                     <tr>
-                        <td><p>Responsable:</p></td>
+                        <td><p>Nombre Cliente:</p></td>
                         <td><input 
-                            name="responsable" 
+                            name="nombre_cliente" 
                             className="input_m" 
                             type="text"
-                            placeholder="responsable" 
+                            placeholder="Nombre Cliente"
                             required/>
                         </td>
+                    </tr>
+
+                    <tr>
+                        <td><p>Vendedor:</p></td>
+                        <td><input 
+                            name="vendedor" 
+                            className="input_m" 
+                            type="text"
+                            placeholder="Vendedor"
+                            required/>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td><p>Estado Venta:</p></td>                
+                        <td><p>
+                            <select 
+                                className="select"
+                                name="estado" 
+                                id="estado" required>
+                                    <option selected disabled value="">Seleccione</option>
+                                    <option value="en_proceso">En Proceso</option>
+                                    <option value="cancelada">Cancelada</option>
+                                    <option value="entregada">Entregada</option>
+                            </select>
+                        </p></td> 
                     </tr>
                     
                     <tr>
