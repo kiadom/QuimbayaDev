82f37df050c1ac1e9972996361b0f5794bded8d0
JuanMa <ingjuanmasuarez@gmail.com>
2021-10-21 08:15:02 -0500

    Despliegue en Heroku

--- backend/server.js
+++ backend/server.js
@@ -15,11 +15,20 @@ const app = express(); //Inicializacion de express
 app.use(bodyParser.json()); //Metodo para trabajar exclusivo con ficheros json
 app.use(bodyParser.urlencoded({extended: false}));
 
+//Se define el puerto de salida local
+const port = 3001;
+
 //Evita el problema CORS al momento de mandar peticiones
 router(app.use(cors()));
 
-//Ejecutar la aplicacion en el puerto 3001
-app.listen(3001);
+app.set('port', process.env.PORT || port);
 
-//Mensaje en consola para verificar que la aplicacion esta iniciada
-console.log('La aplicacion esta escuchando en http://localhost:3001');
+app.get('/', (req, res) =>{
+    res.json("Backend QuimbayaDev");
+})
+
+//Ejecutar la aplicacion en el puerto definido previamente en la variable puerto
+app.listen(app.get('port'), async () => {
+    //Mensaje en consola para verificar que la aplicacion esta iniciada
+    console.log('La aplicacion esta escuchando en http://localhost:' + app.get('port'));
+});
