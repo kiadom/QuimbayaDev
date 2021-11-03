b0a206c80118293d964317fe209332c61cfe3ef4
nacuervo <nelsoncuervo86@gmail.com>
2021-11-03 07:08:39 -0500

    agregar Link para ir al home

--- pages/admin/usuarios/index.tsx
+++ pages/admin/usuarios/index.tsx
@@ -1,10 +1,14 @@
+import Link from 'next/link'
 import React from 'react'
 
 
 const index = () => {
     return (
         <div>
-            <p>Pagina admin de usuarios</p>
+           <div>Pagina de admin usuarios</div>
+            <Link href='/'>
+                <a className="cursor-pointer">Ir al Home</a>
+            </Link>
         </div>
     )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link';
 const Home: NextPage = () => {
   return <div className="bg-red-500">
     <p>Pagina de Index</p>
-    <Link href="/admin/usuarios">
+    <Link href='/admin/usuarios'>
       <a className="cursor-pointer">Ir a admin usuarios</a>
     </Link>
   </div>;
